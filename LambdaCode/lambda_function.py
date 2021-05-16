fd373f880ef0875bdfd423f0b8fcd4645472a658
stvass <steve.vass@gmail.com>
2021-05-16 13:43:33 -0400

    pk lets fucking go

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -2,7 +2,7 @@
 import boto3
 import json
 
-ec2 = boto3.client('ec2')
+//ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
+    response = "hello"
     return {"statusCode": 200, "body": json.dumps(response)}
