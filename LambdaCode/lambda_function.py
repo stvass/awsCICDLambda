24e811bafd901cd5316da97a1693443f7c8efa7e
stvass <steve.vass@gmail.com>
2021-05-16 14:47:33 -0400

    removed the ec2 status premissions issue

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -6,5 +6,5 @@ import json
 ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
     print("hi")
-    response = ec2.describe_availability_zones()
+    response = 'Hello world'
     return {"statusCode": 200, "body": json.dumps(response)}
