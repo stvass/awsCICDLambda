3247b641f70e375517de2e16fd92122b9dd5eab7
stvass <steve.vass@gmail.com>
2021-05-16 13:55:06 -0400

    changed python version fuck

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -2,7 +2,7 @@
 import boto3
 import json
 
-//ec2 = boto3.client('ec2')
+ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
     response = "hello"
     return {"statusCode": 200, "body": json.dumps(response)}
