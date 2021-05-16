f50141cc591d1c435ce6b8c64532f0396625b3a6
stvass <steve.vass@gmail.com>
2021-05-16 14:39:11 -0400

    corrected some code

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -6,5 +6,5 @@ import json
 ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
     print("hi")
-    response = response = ec2.describe_availability_zones()
+    response = ec2.describe_availability_zones()
     return {"statusCode": 200, "body": json.dumps(response)}
