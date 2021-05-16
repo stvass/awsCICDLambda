fd75372e5205980bf4cd88e965eaa10db3804fa5
stvass <steve.vass@gmail.com>
2021-05-16 14:21:19 -0400

    added print

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -2,7 +2,9 @@
 import boto3
 import json
 
+
 ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
+    print("hi")
     response = "hello"
     return {"statusCode": 200, "body": json.dumps(response)}
